6274f428c7a070aae8b03a98682dbfa2d301564a
Kyle Appelgate <grenappels@gmail.com>
2016-09-07 12:08:44 -0400

    worked out a smother keyboard input

---
+++ Assets/MotoInputVisualize.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class MotoInputVisualize : MonoBehaviour
+{
+    MotoInput motoInput;
+    public Transform input;
+    public Transform followedInput;
+    public Transform easedFollowInput;
+    public float movementDistance = 2f;
+
+    void Start()
+    {
+        motoInput = FindObjectOfType<MotoInput>();
+    }
+
+    void Update()
+    {
+        input.localPosition = new Vector3(
+            Input.GetAxisRaw("Horizontal") * movementDistance,
+            input.localPosition.y,
+            input.localPosition.z);
+
+        followedInput.localPosition = new Vector3(
+            motoInput.GetFollowInput() * movementDistance,
+            followedInput.localPosition.y,
+            followedInput.localPosition.z);
+
+        easedFollowInput.localPosition = new Vector3(
+            motoInput.GetEasedInput() * movementDistance,
+            easedFollowInput.localPosition.y,
+            easedFollowInput.localPosition.z);
+    }
+}
---
+++ Assets/Player Controls/MotoInput.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class MotoInput : MonoBehaviour
+{
+    [Range(0.1f, 100f)]
+    public float followSpeed = 1f;
+    [Range(0.1f, 100f)]
+    public float easedSpeed = 1f;
+    float followInput;
+    float easedFollowInput;
+
+    //Note: This script runs first in script execution order.
+    void Update()
+    {
+        var input = Input.GetAxisRaw("Horizontal");
+
+        var followMov = followSpeed * Time.deltaTime;
+        followInput = Mathf.MoveTowards(followInput, input, followMov);
+
+        var easedMov = easedSpeed * Time.deltaTime;
+        easedMov *= Mathf.Abs(followInput - easedFollowInput);
+        easedMov = Mathf.Max(0.0001f, easedMov);
+        easedFollowInput = Mathf.MoveTowards(easedFollowInput, followInput, easedMov);
+    }
+
+    public float GetFollowInput()
+    {
+        return followInput;
+    }
+
+    public float GetEasedInput()
+    {
+        return easedFollowInput;
+    }
+}
