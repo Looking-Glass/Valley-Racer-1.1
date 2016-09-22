25c699ab91f236f535c7b6329d5d835da556847e
Kyle Appelgate <grenappels@gmail.com>
2016-09-22 09:19:27 -0400

    hye

--- Assets/Animator/Crash Scenewide Anim/CrashScenewideAnim.cs
+++ Assets/Animator/Crash Scenewide Anim/CrashScenewideAnim.cs
@@ -11,6 +11,8 @@ public class CrashScenewideAnim : MonoBehaviour
     public hypercubeCamera hypercube;
     public float hypercubeTubeFactor;
     public float hypercubePerspectiveFactor;
+    public ExplosionController explosionController;
+    public FollowBiker followBiker;
     Animator animator;
 
     void Start()
@@ -31,21 +33,42 @@ public class CrashScenewideAnim : MonoBehaviour
         var initHypercubeRot = hypercubeSurrogatePair[0].rotation;
         var initHypercubeScale = hypercubeSurrogatePair[0].localScale;
 
+        var initBikerPos = bikerSurrogatePair[0].position;
+        var initBikerRot = bikerSurrogatePair[0].rotation;
+
         yield return new WaitForEndOfFrame();
 
         while (animator.GetCurrentAnimatorStateInfo(0).IsName("CrashAnim") &&
             animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1f)
         {
             yield return new WaitForEndOfFrame();
+            hypercubeSurrogatePair[0].position = initHypercubePos + hypercubeSurrogatePair[1].position;
+            hypercubeSurrogatePair[0].rotation = initHypercubeRot * hypercubeSurrogatePair[1].rotation;
+            hypercubeSurrogatePair[0].localScale = Vector3.Scale(initHypercubeScale, hypercubeSurrogatePair[1].localScale);
+
+            bikerSurrogatePair[0].position = initBikerPos + bikerSurrogatePair[1].position;
+            bikerSurrogatePair[0].rotation = initBikerRot * bikerSurrogatePair[1].rotation;
+
+            hypercube.tubeFactor = hypercubeTubeFactor;
+            hypercube.perspectiveFactor = hypercubePerspectiveFactor;
         }
+
+        followBiker.enabled = false;
+
+        hypercubeSurrogatePair[0].position = initHypercubePos + hypercubeSurrogatePair[1].position;
+        hypercubeSurrogatePair[0].rotation = initHypercubeRot * hypercubeSurrogatePair[1].rotation;
+        hypercubeSurrogatePair[0].localScale = Vector3.Scale(initHypercubeScale, hypercubeSurrogatePair[1].localScale);
+
+        bikerSurrogatePair[0].position = initBikerPos + bikerSurrogatePair[1].position;
+        bikerSurrogatePair[0].rotation = initBikerRot * bikerSurrogatePair[1].rotation;
+
+        hypercube.tubeFactor = hypercubeTubeFactor;
+        hypercube.perspectiveFactor = hypercubePerspectiveFactor;
     }
 
-    //Comment this out when not editing.
-    void Update()
+    void Explode()
     {
-        hypercubeSurrogatePair[0].position = hypercubeSurrogatePair[1].position;
-        hypercubeSurrogatePair[0].rotation = hypercubeSurrogatePair[1].rotation;
-        hypercubeSurrogatePair[0].localScale = hypercubeSurrogatePair[1].localScale;
+        explosionController.Explode();
     }
 
     void OnDestroy()
---
+++ Assets/CrashSequence.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashSequence : MonoBehaviour
+{
+    public ExplosionController explosion;
+    public CameraShake cameraShake;
+
+    void Start()
+    {
+        EventManager.playerDeath += OnPlayerDeath;
+    }
+
+    void OnPlayerDeath()
+    {
+        explosion.Explode();
+        cameraShake.enabled = true;
+    }
+
+    void OnDestroy()
+    {
+        EventManager.playerDeath -= OnPlayerDeath;
+    }
+}
